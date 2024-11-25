626b57458881c24e9a5d3b754a30c1b3ff3a82dd
Aryan Singh <66953279+Aryan2001-Singh@users.noreply.github.com>
2024-11-25 12:59:32 +0530

    Update page.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -68,7 +68,7 @@ const OrganizationIdPage = () => {
     }
 
     try {
-      const response = await fetch("https://kogenie-server-f078f630bb40.herokuapp.com/createAd", {
+      const response = await fetch("https://kogenie-backend-0cd1c9313886.herokuapp.com/createAd", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url, gender, ageGroup }), // Send gender and ageGroup along with the URL
