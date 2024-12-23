6fa7ac4b9a749b100ce8c2c348f000ce2d734b4f
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2024-12-23 13:04:11 +0530

    xml file added with robot.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://www.kogenie.com/', // Replace with your actual site URL
+    generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+    sitemapSize: 5000, // (Optional) Split sitemap if there are many URLs
+  };
