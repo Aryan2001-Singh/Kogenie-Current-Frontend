05ea3d658463c545e646ccb10648a9da0a8b16ea
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2025-02-04 18:20:04 +0530

    updated sitemap

--- app/(platform)/(dashboard)/organization/[organizationId]/createAd/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/createAd/page.tsx
@@ -175,7 +175,7 @@ const CreateAdPage: React.FC = () => {
       </button> */}
 <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Create Your Post</h1>
-      <UploadForm />
+
     </div>
 
     </div>
